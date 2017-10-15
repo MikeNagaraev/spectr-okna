d29ae9ee0abe2ce84e100d35c527d35b6ba774e1
MikhailLU <clowfinger13@gmail.com>
2017-10-15 14:49:52 +0300

    webpack installed / sass and es6 plugins installed

---
+++ webpack.config.babel.js
@@ -0,0 +1,88 @@
+import webpack from 'webpack';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const jsOutputPath = 'js/app.js';
+const cssOutputPath = 'css/app.min.css';
+
+const ExtractSASS = new ExtractTextPlugin(cssOutputPath);
+
+export default {
+  entry: [
+    './src/es6/app.js',
+    './src/sass/app.scss',
+  ],
+  output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: jsOutputPath,
+  },
+  resolve: {
+    extensions: ['.js', '.scss', '.css']
+  },
+  devtool: 'source-map',
+  watch: true,
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: [
+                'transform-runtime',
+                'transform-object-rest-spread'
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'src/sass'),
+        ],
+        use: ExtractSASS.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                minimize: true,
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: "style-loader"
+        }),
+      },{
+        test: /\.css$/,
+        include: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'src/sass'),
+        ],
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin(path.resolve(__dirname, `public/${cssOutputPath}`), {
+        allChunks: true,
+    }),
+    ExtractSASS,
+  ],
+};
